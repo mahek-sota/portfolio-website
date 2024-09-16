d3aa68edd84ea5b3c5b63485eef1e90ea5b283d4
Mahek Sota <maheksota@hst000261465um.nat.olemiss.edu>
2024-09-16 10:22:43 -0500

    updates on projects about links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css'; // Ensure FontAwesome CSS is imported
 
 const projectsList = [
-  { id: 1, name: 'Event Finder: React Web Application and Android Application', description: 'Developed a React web and Android app integrating Ticketmaster, IPinfo, and Spotify APIs for event discovery and user favorites.', link: 'http://example.com/project-one', github: 'https://github.com/mahek-sota/event-finder', techstack: 'HTML, CSS, JS, React, NodeJS, RESTFul API, localStorage, GIT, GCP, Bootstrap, Android Studio, Java.' },
-  { id: 2, name: 'Work It: Fitness Web Application', description: 'Led the creation of a fitness web app with a chatbot, video calls, and a recipe recommender, achieving high accuracy in both chatbot and recommendation systems.', link: 'http://example.com/project-two', github: 'https://github.com/mahek-sota/work-it', techstack: 'HTML, CSS, JS, React, Flask, ChatterBot, BeautifulSoup, NLP techniques - cosine similarity, Word2Vec.' },
-  { id: 3, name: 'RNN and LSTM Model for Sentimental Analysis', description: 'Built an NLP model for Amazon review sentiment analysis using RNN and LSTM', link: 'http://example.com/project-three', github: 'https://github.com/mahek-sota/sentimental-analysis', techstack: 'Python, PyTorch, Word2Vec, RNN, LSTM' },
+  { id: 1, name: 'Event Finder: React Web Application and Android Application', description: 'Developed a React web and Android app integrating Ticketmaster, IPinfo, and Spotify APIs for event discovery and user favorites.', link: 'https://docs.google.com/document/d/1u9q8XAGkUmgBxLdAOcj9jyr0kd25T4P_pdSrbn-EWwg/edit?usp=sharing', github: 'https://github.com/mahek-sota/event-finder', techstack: 'HTML, CSS, JS, React, NodeJS, RESTFul API, localStorage, GIT, GCP, Bootstrap, Android Studio, Java.' },
+  { id: 2, name: 'Work It: Fitness Web Application', description: 'Led the creation of a fitness web app with a chatbot, video calls, and a recipe recommender, achieving high accuracy in both chatbot and recommendation systems.', link: 'https://docs.google.com/document/d/1tFukSwe_yZy_knFCihmwj_MQRzO0JGtGtfktZgHlx4s/edit?usp=sharing', github: 'https://github.com/mahek-sota/work-it', techstack: 'HTML, CSS, JS, React, Flask, ChatterBot, BeautifulSoup, NLP techniques - cosine similarity, Word2Vec.' },
+  { id: 3, name: 'RNN and LSTM Model for Sentimental Analysis', description: 'Built an NLP model for Amazon review sentiment analysis using RNN and LSTM', link: 'https://docs.google.com/document/d/1U5hXw_6HgvX2yfLaGD7TgUyDL6GHvq4cxx76GrUnVRs/edit?usp=sharing', github: 'https://github.com/mahek-sota/sentimental-analysis', techstack: 'Python, PyTorch, Word2Vec, RNN, LSTM' },
   // Add more projects as needed
 ];
+
 
 const ProjectsCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
