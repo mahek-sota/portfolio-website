fe654ec32d3c43aead9bc1ef850efe109667c5b3
mahek sota <maheksota56@gmail.com>
2025-03-04 16:36:21 -0600

    removed all

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/portfolio-website/', // Ensure this matches your repository name
-  plugins: [react()],
-});
